054792ddd9c7805983c466453f3c3157925ac96e
Ryan Tomas <113748661+Rhino004@users.noreply.github.com>
2024-06-06 00:31:31 -0700

    fixing tabs text size on phone

--- src/functions/Button.js
+++ src/functions/Button.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import './Functions.css'
+import { isMobile } from 'react-device-detect'
 
 const Button = ({href,text, onClick}) => {
-  
+  if (isMobile)
+  {
+    return (
+      <button 
+      className='btn_mobile'
+      onClick = {onClick}>
+        {text}
+        </button>
+    )
+  }
+
   return (
     <button 
     className='btn'
